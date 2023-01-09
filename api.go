a99c1ce8a515116ed9960e8e6743a8f9c33b59cb
fu <yanyuan@g.ecc.u-tokyo.ac.jp>
2023-01-09 13:48:33 +0900

    fix: add unit test

---
+++ api.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/spruceid/siwe-go"
+)
+
+type verifyReq struct {
+	Message   string `json:"message"`
+	Signature string `json:"signature"`
+}
+
+type errorResp struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type nonceResp struct {
+	Nonce string `json:"nonce"`
+}
+
+func nonce(c *gin.Context) {
+	session := sessions.Default(c)
+
+	session.Set("nonce", siwe.GenerateNonce())
+	session.Save()
+
+	nonce := nonceResp{session.Get("nonce").(string)}
+	c.JSON(200, gin.H{
+		"data": nonce,
+	})
+}
+
+func verify(c *gin.Context) {
+	var requestBody verifyReq
+	var err error
+	var message *siwe.Message
+	optionalDomain := "api.soularis.dev"
+
+	//session
+	session := sessions.Default(c)
+
+	c.BindJSON(&requestBody)
+
+	if message, err = siwe.ParseMessage(requestBody.Message); message == nil {
+		errInfo := errorResp{Code: 123, Message: err.Error()}
+		c.JSON(422, gin.H{
+			"error": errInfo,
+		})
+		return
+	}
+
+	// VERIFY
+	optionalNonce, _ := session.Get("nonce").(string)
+	_, err = message.Verify(requestBody.Signature, &optionalDomain, &optionalNonce, nil)
+
+	if err != nil {
+		errInfo := errorResp{Code: 123, Message: err.Error()} // error code not determined
+		c.JSON(422, gin.H{
+			"error": errInfo,
+		})
+		return
+	}
+
+	c.Status(204)
+}
+
+func main() {
+	r := gin.Default()
+
+	//session
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/nonce", nonce)
+	r.POST("/verify", verify)
+
+	r.Run(":8080") //listen and serve on :8080
+}
---
+++ api_test.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/spruceid/siwe-go"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/assert"
+)
+
+const testDomain = "api.soularis.dev"
+
+const testStatement = "Test statement for SIWE"
+const testUri = "https://example.com"
+
+var testIssuedAt = time.Now().UTC().Format(time.RFC3339)
+var testExpirationTime = time.Now().UTC().Add(48 * time.Hour).Format(time.RFC3339)
+
+const testChainId = 1
+
+var testOptions = map[string]interface{}{
+	"statement":      testStatement,
+	"chainId":        testChainId,
+	"issuedAt":       testIssuedAt,
+	"expirationTime": testExpirationTime,
+	// "notBefore":      nil,
+	// "requestId":      nil,
+	// "resources":      nil,
+}
+
+type RespStruct struct {
+	Data nonceResp `json:"data"`
+}
+
+func signHash(data []byte) common.Hash {
+	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)
+	return crypto.Keccak256Hash([]byte(msg))
+}
+
+func signMessage(message string, privateKey *ecdsa.PrivateKey) ([]byte, error) {
+	sign := signHash([]byte(message))
+	signature, err := crypto.Sign(sign.Bytes(), privateKey)
+
+	if err != nil {
+		return nil, err
+	}
+
+	signature[64] += 27
+	return signature, nil
+}
+
+func TestSIWE(t *testing.T) {
+	//#1 KEYPAIR
+	privateKey, _ := crypto.GenerateKey()
+	publicKey := privateKey.PublicKey
+	testAddress := crypto.PubkeyToAddress(publicKey)
+
+	//#2 GET NONCE
+	w := httptest.NewRecorder()
+	r := gin.Default()
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/nonce", nonce)
+	r.POST("/verify", verify)
+
+	req := httptest.NewRequest("GET", "/nonce", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	nonceData := RespStruct{}
+	respBody := w.Result().Body
+	cookie := w.Result().Header.Get("Set-Cookie")
+	defer respBody.Close()
+	body, _ := io.ReadAll(respBody)
+
+	json.Unmarshal(body, &nonceData) // for w.Result().Body
+
+	//#3 MESSAGE STRUCT
+	testMessage, _ := siwe.InitMessage(
+		testDomain,
+		testAddress.String(),
+		testUri,
+		nonceData.Data.Nonce,
+		testOptions,
+	)
+	testMessageStr := testMessage.String()
+
+	//#4 Signature
+	testSignature, _ := signMessage(testMessageStr, privateKey)
+	testSignatureStr := hexutil.Encode(testSignature)
+
+	reqBody := verifyReq{
+		Message:   testMessageStr,
+		Signature: testSignatureStr,
+	}
+	reqBodyJson, _ := json.Marshal(reqBody)
+
+	w = httptest.NewRecorder()
+	req = httptest.NewRequest("POST", "/verify", bytes.NewBuffer(reqBodyJson))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", cookie)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 204, w.Code)
+}
